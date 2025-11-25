1171b70bb3b20e5cf5defd881352a2e1c88f2bec
你的名字 <zxia@nyit.edu>
2025-11-25 14:41:44 -0500

    Finalized Validators and added button for additem and its page layout

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget{
   const NewItem({super.key});
   State<NewItem> createState(){
     return _NewItemState();
   }
-
 }
 
 class _NewItemState extends State<NewItem>{
@@ -18,6 +18,68 @@ class _NewItemState extends State<NewItem>{
       ),
       body:Padding(
         padding:const EdgeInsets.all(12),
+         child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: InputDecoration(label: Text('Name')),
+                validator: (value){
+                  if(value==null || value.isEmpty || value.length <=1 || value.length > 50)
+                  {
+                    return "Must have a name between 2 and 50 characters long!";
+                  }
+                 return null;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      label:Text("Quantity"),
+                    ),
+                    initialValue: '1',
+                    validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value)==null ||
+                            int.tryParse(value)! <=0) {
+                          return "Must Enter a quantity greater than 0";
+                        }
+                        return null;
+                      },
+                  ),
+                ),
+                const SizedBox(width:8),
+                Expanded(
+                  child: DropdownButtonFormField(items: [
+                    for(final category in categories.entries)
+                      DropdownMenuItem(
+                        value:category.value,
+                        child: Row(children: [
+                        Container(
+                          width:16, 
+                          height:16,
+                          color:category.value.color
+                        ),
+                        const SizedBox(width:6),
+                        Text(category.value.title),
+                      ],)
+                      ,)
+                  ], onChanged: (value){}),
+                )
+              ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                TextButton(onPressed: (){}, child: const Text("Reset")),
+                ElevatedButton(onPressed: (){}, child: Text("Add Item"))
+              ],)
+            ],
+          ),
+        ),
       ),
     );
   }
